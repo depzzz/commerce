c24c0c420a0dab071205bc4fa1cb7b4b6dfb23c3
Depzzz <depanshu.aroraa@gmail.com>
2021-05-30 16:30:36 +0530

    Completed Specification 2 - Create Listing Users should be able to visit a page to create a new listing. They should be able to specify a title for the listing, a text-based description, and what the starting bid should be. Users should also optionally be able to provide a URL for an image for the listing and/or a category (e.g. Fashion, Toys, Electronics, Home, etc.).

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 from .models import listings, bids, comments, watchlist, winner
 
+#For the sake of showing readonly_fields into django admin interface
+class listingsread(admin.ModelAdmin):
+    fields = ['id', 'title', 'description', 'owner', 'starting_price', 'image_url', 'category', 'status', 'added_on']
+    readonly_fields = ['added_on','id']
+
+class bidsread(admin.ModelAdmin):
+    fields = ['id','user', 'listingid', 'title', 'bid_amount','last_modified']
+    readonly_fields = ['id','last_modified']
+
+class commentsread(admin.ModelAdmin):
+    fields = ['id','user', 'listingid', 'comment', 'added_on']
+    readonly_fields = ['id','added_on']
+
+class watchlistread(admin.ModelAdmin):
+    fields = ['id','user', 'listingid', 'added_on']
+    readonly_fields = ['id','added_on']
+
+class winnerread(admin.ModelAdmin):
+    fields = ['id','winner', 'owner', 'listingid', 'title','bid_amount','timestamp']
+    readonly_fields = ['id','timestamp']
+
 # Register your models here.
-admin.site.register(listings)
-admin.site.register(bids)
-admin.site.register(comments)
-admin.site.register(watchlist)
-admin.site.register(winner)
+admin.site.register(listings,listingsread)
+admin.site.register(bids,bidsread)
+admin.site.register(comments,commentsread)
+admin.site.register(watchlist,watchlistread)
+admin.site.register(winner,winnerread)
--- auctions/views.py
+++ auctions/views.py
@@ -3,8 +3,30 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django import forms
+from .models import User, listings
 
-from .models import User
+# Categories for Listing Creation
+CATEGORIES = [
+    ("ENTERTAINMENT", "Entertainment"),
+    ("ELECTRONICS", "Electronics"),
+    ("FASHION", "Fashion"),
+    ("GENERAL", "General"),
+    ("HOME", "Home"),
+    ("TOYS", "Toys"),
+]
+
+class CreateForm(forms.Form):
+    title = forms.CharField(label='Enter Title',max_length=64)
+    description = forms.CharField(label='Enter Listing Description',max_length=250)
+    starting_price = forms.IntegerField(label='Enter Bid Starting Price',min_value=1)
+    image_url = forms.URLField(label='Enter Image Url',max_length=200)
+    category = forms.ChoiceField(
+        label='Choose a Category',
+        choices= CATEGORIES,
+        widget= forms.RadioSelect
+    )
 
 
 def index(request):
@@ -61,3 +83,31 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required(login_url='login')
+def create_listing(request):
+    if request.method == "POST":
+        # Create a Listing & Add the same to database
+        form = CreateForm(request.POST)
+            
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            starting_price = form.cleaned_data['starting_price']
+            image_url = form.cleaned_data['image_url']
+            category = form.cleaned_data['category']
+
+            new_listing = listings(title=title,
+                                description=description,
+                                starting_price=starting_price,
+                                image_url=image_url,
+                                category=category,
+                                owner=request.user.username)
+            new_listing.save()
+
+            return HttpResponse("Added Successfully!")
+        else:
+            return HttpResponse("Something Wrong Happened!")
+    else:
+        context = {"CreateForm" : CreateForm}
+        return render(request, "auctions/create.html", context)
