2c245c3f32ef05538aef18b045732cfad331ce16
Depzzz <depanshu.aroraa@gmail.com>
2021-05-30 17:14:33 +0530

    Completed Specification 3 - Active Listings The default route of your web application should let users view all of the currently active auction listings. For each active listing, this page should display (at minimum) the title, description, current price, and photo (if one exists for the listing).

--- auctions/views.py
+++ auctions/views.py
@@ -17,6 +17,7 @@ CATEGORIES = [
     ("TOYS", "Toys"),
 ]
 
+# form for creating of a new listing
 class CreateForm(forms.Form):
     title = forms.CharField(label='Enter Title',max_length=64)
     description = forms.CharField(label='Enter Listing Description',max_length=250)
@@ -28,14 +29,15 @@ class CreateForm(forms.Form):
         widget= forms.RadioSelect
     )
 
-
+# default route index for showing all the active listings
 def index(request):
-    return render(request, "auctions/index.html")
+    # Get active listings from the listings table
+    context = {"listings": listings.objects.filter(status='O')}
+    return render(request, "auctions/index.html",context)
 
-
+# login page route
 def login_view(request):
     if request.method == "POST":
-
         # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
@@ -52,12 +54,12 @@ def login_view(request):
     else:
         return render(request, "auctions/login.html")
 
-
+# logout route, for logging the user out
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
-
+# register route, for creation of a new user
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -84,12 +86,14 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+# create route, renders a form for new listing creation and saves it into db
 @login_required(login_url='login')
 def create_listing(request):
     if request.method == "POST":
         # Create a Listing & Add the same to database
         form = CreateForm(request.POST)
-            
+
+        # Get Data From Form & Check whether it is valid 
         if form.is_valid():
             title = form.cleaned_data['title']
             description = form.cleaned_data['description']
@@ -97,6 +101,7 @@ def create_listing(request):
             image_url = form.cleaned_data['image_url']
             category = form.cleaned_data['category']
 
+            # insert the data into listings table
             new_listing = listings(title=title,
                                 description=description,
                                 starting_price=starting_price,
@@ -105,9 +110,13 @@ def create_listing(request):
                                 owner=request.user.username)
             new_listing.save()
 
+            # go back to the listing page and 
+            # show an alert that the listing was added successfully
             return HttpResponse("Added Successfully!")
         else:
+            # show validation errors of forms
             return HttpResponse("Something Wrong Happened!")
     else:
+    # if the request is get then render create listing form
         context = {"CreateForm" : CreateForm}
         return render(request, "auctions/create.html", context)
